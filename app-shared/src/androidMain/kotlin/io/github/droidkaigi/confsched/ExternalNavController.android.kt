933ff9d5d26b65b29cae5131e933b49860efae08
yoshida0261 <yoshida261@gmail.com>
2025-09-11 23:57:16 +0900

    Add hashtag

--- app-shared/src/androidMain/kotlin/io/github/droidkaigi/confsched/ExternalNavController.android.kt
+++ app-shared/src/androidMain/kotlin/io/github/droidkaigi/confsched/ExternalNavController.android.kt
@@ -83,6 +83,7 @@ class AndroidExternalNavController(
         val text = "[${timetableItem.room.name.currentLangTitle}] ${timetableItem.formattedMonthAndDayString} " +
             "${timetableItem.startsTimeString} - ${timetableItem.endsTimeString}\n" +
             "${timetableItem.title.currentLangTitle}\n" +
+            "#Droidkaigi\n" +
             timetableItem.url
         try {
             ShareCompat.IntentBuilder(context)
